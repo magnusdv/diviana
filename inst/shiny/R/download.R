62124707c8f40283c8a659f2b522dde3fb880bcd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-06-11 08:26:10 +0200

    Fix sheet names and colWidths in excel download

--- inst/shiny/R/download.R
+++ inst/shiny/R/download.R
@@ -11,16 +11,34 @@ downloadTables = function(results, filename) {
 
   results$LR[] = safeFormat(results$LR)
 
-  if(length(results$JT))
-    results$JT = results$JT[[1]] # TODO: fix to handle > 1
+  # TODO: fix to handle > 1 joint families
+  if(length(JT <- results$JT)) {
+    JT = JT[[1]]
+    if(nrow(JT) > 1e3)
+      JT = JT[1:1e3, , drop = FALSE]
+    results$JT = JT
+  }
 
-  names(results) = c("resultsAM", "resultsPM", "LR matrix", "Exclusions", "Joint")
+  #names(results) = c("resultsAM", "resultsPM", "LR matrix", "Exclusions", "Joint")
+
+  nLR = ncol(results$LR)
+  nEX = ncol(results$EX)
 
   hs = openxlsx::createStyle(textDecoration = "bold")
-  wb = openxlsx::buildWorkbook(results, headerStyle = hs, colWidths = "auto",
-                               rowNames = list(FALSE,FALSE,TRUE,TRUE, FALSE))
-  openxlsx::addStyle(wb, 3, cols = 1, rows = 1+seq_len(nvic), style = hs, stack = TRUE)
-  openxlsx::addStyle(wb, 4, cols = 1, rows = 1+seq_len(nvic), style = hs, stack = TRUE)
+  hsc = openxlsx::createStyle(textDecoration = "bold", halign = "center")
+
+  wb = openxlsx::buildWorkbook(results, headerStyle = hs,
+                               rowNames = list(FALSE, FALSE, TRUE, TRUE, FALSE))
+
+  openxlsx::setColWidths(wb, 1, cols = 1:ncol(results[[1]]), widths = "auto")
+  openxlsx::setColWidths(wb, 2, cols = 1:ncol(results[[2]]), widths = "auto")
+  openxlsx::setColWidths(wb, 3, cols = 1:(nLR + 1), widths = 12)
+  openxlsx::setColWidths(wb, 4, cols = 1:(nEX + 1), widths = 6)
+
+  # Center matrix headers
+  for(j in 3:4) {
+    openxlsx::addStyle(wb, j, cols = seq_len(nLR + 1), rows = 1, style = hsc, stack = TRUE)
+  }
 
   openxlsx::saveWorkbook(wb, file = filename, overwrite = TRUE)
   invisible(wb)
@@ -29,6 +47,6 @@ downloadTables = function(results, filename) {
 
 safeFormat = function(v, ...) {
   res = format(v, ...)
-  res[res == "NA"] = ""
+  res[is.na(v) | trimws(res) == "NA"] = ""
   res
 }
